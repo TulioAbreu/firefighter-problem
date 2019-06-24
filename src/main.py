82ec0aada8ad77a502dc626ba2027d8916a9377c
TulioAbreu <marco-tulioabreu1@hotmail.com>
2019-06-23 22:25:14 -0300

    Final changes

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import time
 
 def runInstance(instanceNumber):
     instance = Instance()
+    print('Reading file ../instances/%s.txt' % str(instanceNumber))
     instance.readInstance('../instances/%s.txt' % str(instanceNumber))
 
     start_time = time.time()
@@ -14,12 +15,14 @@ def runInstance(instanceNumber):
     instance.printReport()
     print('Time = %s seconds' % str(finish_time-start_time))
     assert (finish_time-start_time) < 600.0  # 10 min time limit
+    return (instance, finish_time-start_time)
 
 
 def selectDefenseVertex(instance:Instance) -> [int]:
     """
         This function defines how to select the vertex to defend on each round
     """
+    # return [int(input("Digite um numero para defesa: "))]
     return selector.MiniMaxSelector(instance, 3).selectDefenseVertex()
     # return [selector.RandomSelector(instance).selectDefenseVertex()]
 
@@ -28,24 +31,56 @@ def solve(instance:Instance):
     """
         This function is called to solve an instance
     """
-    print ('Grafo com %s vertices' % instance.getGraphVertexCount())
+    print ('Graph has %s vertices' % instance.getGraphVertexCount())
+
     i = 0
     toBlock = []
     while True: # Finishes when there is no changes between two rounds
         if instance.getVertexCounterByState(State.UNTOUCHED) > 0:
-            if len(toBlock) > 0:
-                instance.protectVertex(toBlock.pop(0))
-            else:
-                indexesToBlock = selectDefenseVertex(instance)
-                instance.protectVertex(indexesToBlock.pop(0))
-                [toBlock.append(index) for index in indexesToBlock]
+            for i in range(5):
+                if len(toBlock) > 0:
+                    instance.protectVertex(toBlock.pop(0))
+                else:
+                    indexesToBlock = selectDefenseVertex(instance)
+                    if indexesToBlock != []:
+                        instance.protectVertex(indexesToBlock.pop(0))
+                        # instance.protectVertex(indexesToBlock.pop(0))
+                        [toBlock.append(index) for index in indexesToBlock]
         if instance.nextRound() is not True:
             break
         i += 1
 
 
+def saveCSV(results):
+    file = open("../output.csv", "w")
+    file.write('Instância; Vertices Queimados; Total de Vertices; Defendidos por Round; Tempo\n')
+    for i, (instance, time) in enumerate(results):
+        line = '%s; %s; %s; %s; %s\n' % (
+            str(i+1),
+            instance.getVertexCounterByState(State.BURNT),
+            instance.getGraphVertexCount(),
+            defPerRoundScript(instance),
+            str(time)
+        )
+        file.write(line)
+    file.close()
+
+
+def defPerRoundScript(instance:Instance):
+    numRounds = max(instance.report, key=lambda x:x['round'])['round']
+    finalStr = ""
+    for i in range(numRounds):
+        roundNum = i+1
+        protected = [str(e['index']) for e in instance.report if e['round'] == roundNum]
+        finalStr += ('Round %s (' % str(roundNum)) + ", ".join(protected) + ')  '
+    return finalStr
+    # print("Vertex %s deffended in round %s" % (defVertex['index'], defVertex['round']))
+
+
 if __name__ == "__main__":
+    results = list()
     for i in range(1, 12):
         print ('## Instance %s' % i)
-        runInstance(9)
-        print ('############################')
+        results.append(runInstance(i))
+        print ('###################################################################################################')
+    saveCSV(results)
--- src/selector.py
+++ src/selector.py
@@ -26,44 +26,33 @@ class RandomSelector(Selector):
 
 
 import copy
+from math import inf
 class MiniMaxSelector(Selector):
     def __init__(self, instance:Instance, maxDepth:int):
         super().__init__(instance)
         self.maxDepth = maxDepth
         self.maximizingPlayer = False
-        # alpha = float('-inf')
-        # beta = float('inf')
-
-    # def isTerminalNode(self):
-    #     return self.instance.getVertexCounterByState(State.UNTOUCHED) == 0
-
-    # def getChildNodes(self, node:Instance):
-    #     childNodes = list()
-    #     untouchedVertices = node.filterUntouchedVertices() # Options to play
-    #     minValue = float('inf')
-    #     for untouchedVertex in untouchedVertices:
-    #         nodeCopy = copy.deepcopy(node)
-    #         nodeCopy.protectVertex(untouchedVertex)
-    #         nodeCopy.nextRound()
-    #         childNodes.append((nodeCopy, untouchedVertex))
-    #         nodeValue = nodeCopy.getHeuristic()
-    #         if nodeValue <= minValue:
-    #             minValue = nodeValue
-    #     return [node for node in childNodes if node[0].getHeuristic() <= minValue]
+        self.counter = 5
+        self.alpha = -inf
+        self.beta = inf
 
     def isGameOver(self, node:Instance):
         nodeCopy = copy.deepcopy(node)
         return nodeCopy.nextRound() is False
 
-    def minimax(self, node:Instance, depth:int, path:[int], isMaximizingPlayer:bool, alpha, beta):
+    def minimax(self, node:Instance, depth:int, path:[int], isMaximizingPlayer:bool):
+    # def minimax(self, node:Instance, depth:int, path:[int], isMaximizingPlayer:bool, alpha, beta):
         if depth == 0 or self.isGameOver(node):
             return node.getHeuristic(), path
 
         if isMaximizingPlayer:
             childNode = copy.deepcopy(node)
             childNode.nextRound()
-            value, newPath = self.minimax(childNode, depth-1, path, False, alpha, beta)
-            alpha = max(alpha, value)
+            self.counter = 5
+            # value, newPath = self.minimax(childNode, depth-1, path, False, alpha, beta)
+            value, newPath = self.minimax(childNode, depth-1, path, False)
+            # alpha = max(alpha, value)
+            self.alpha = max(self.alpha, value)
             return value, newPath
         else:
             bestValue = float('inf')
@@ -73,19 +62,27 @@ class MiniMaxSelector(Selector):
             for childNodePlay in childNodePlays:
                 childNode = copy.deepcopy(node)
                 childNode.protectVertex(childNodePlay)
-                value, usedPath = self.minimax(childNode, depth-1, path + [childNodePlay], True, alpha, beta)
+                if self.counter <= 0:
+                    # value, usedPath = self.minimax(childNode, depth-1, path + [childNodePlay], True, alpha, beta)
+                    value, usedPath = self.minimax(childNode, depth-1, path + [childNodePlay], True)
+                else:
+                    self.counter -= 1
+                    # value, usedPath = self.minimax(childNode, depth, path + [childNodePlay], False, alpha, beta)
+                    value, usedPath = self.minimax(childNode, depth, path + [childNodePlay], False)
                 if value < bestValue:
                     bestValue = value
                     pathToUse = usedPath
-                beta = min(beta, value)
-                if alpha >= beta:
-                    print('Corte realizado')
+                # beta = min(beta, value)
+                # if alpha >= beta:
+                self.beta = min(self.beta, value)
+                if self.alpha >= self.beta:
                     break
             return bestValue, pathToUse
 
     def selectDefenseVertex(self):
-        value, optimalPath = self.minimax(self.instance, self.maxDepth, [], False, float('-inf'), float('inf'))
-        print ('Optimal value =', value)
-        print ('Optimal path =', optimalPath)
-        print ('-------------------------------------------------')
+        # value, optimalPath = self.minimax(self.instance, self.maxDepth, [], False, float('-inf'), float('inf'))
+        value, optimalPath = self.minimax(self.instance, self.maxDepth, [], False)
+        # print ('Optimal value =', value)
+        # print ('Optimal path =', optimalPath)
+        # print ('-------------------------------------------------')
         return optimalPath
